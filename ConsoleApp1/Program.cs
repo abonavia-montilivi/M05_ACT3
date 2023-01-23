2b84b732c03af738d595b6c6643a1b67fd43a788
abonavia <a2208025@institutmontilivi.cat>
2023-01-23 17:48:46 +0100

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,96 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConsoleKeyInfo tecla;
+        do
+        {
+            Console.Clear();
+            ShowOptions();
+            tecla = Console.ReadKey();
+            Console.Clear();
+            switch (tecla.Key)
+            {
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    MusicCuriosities();
+                    break;
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    DadJokes();
+                    break;
+                case ConsoleKey.D3:
+                case ConsoleKey.NumPad3:
+                    Science();
+                    break;
+                case ConsoleKey.D4:
+                case ConsoleKey.NumPad4:
+                    DrivingSchool();
+                    break;
+                case ConsoleKey.D5:
+                case ConsoleKey.NumPad5:
+                    Riddles();
+                    break;
+                case ConsoleKey.D6:
+                case ConsoleKey.NumPad6:
+                    Astronomy();
+                    break;
+                case ConsoleKey.D7:
+                case ConsoleKey.NumPad7:
+                    History();
+                    break;
+                case ConsoleKey.D8:
+                case ConsoleKey.NumPad8:
+                    Cinema();
+                    break;
+                case ConsoleKey.D9:
+                case ConsoleKey.NumPad9:
+                    Geography();
+                    break;
+                 case ConsoleKey.D0:
+                case ConsoleKey.NumPad0:
+                    MsgNextScreen("PRESS ANY KEY TO EXIT");
+                    break;
+                default:
+                    MsgNextScreen("Error. Prem una tecla per tornar al menú...");
+                    break;
+            }
+        } while (tecla.Key != ConsoleKey.D0);
+    }
+
+    public static void ShowOptions()
+    {
+        Console.Clear();
+        Console.WriteLine("1 - MUSIC CURIOSITIES");
+        Console.WriteLine("2 - DAD JOKES");
+        Console.WriteLine("3 - SCIENCE");
+        Console.WriteLine("4 - DRIVING SCHOOL");
+        Console.WriteLine("5 - RIDDLES");
+        Console.WriteLine("6 - ASTRONOMY");
+        Console.WriteLine("7 - HISTORY");
+        Console.WriteLine("8 - CINEMA");
+        Console.WriteLine("9 - GEOGRAPHY");
+        Console.WriteLine("0 - EXIT");
+    }
+    public static void MsgNextScreen(string msg)
+    {
+        Console.WriteLine(msg);
+        Console.ReadKey();
+    }
+    //Arnau
+
+
+
+    //Eloy
+
+
+
+
+    //Jaume
+
+
+
+
+
+
+}
