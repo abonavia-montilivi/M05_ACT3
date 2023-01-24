eceba380173fb28fd54e459df6086426a4cf112f
abonavia <a2208025@institutmontilivi.cat>
2023-01-24 18:08:35 +0100

    Master and numpad0Original Master

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -47,7 +47,7 @@
                 case ConsoleKey.NumPad9:
                     Geography();
                     break;
-                 case ConsoleKey.D0:
+                case ConsoleKey.D0:
                 case ConsoleKey.NumPad0:
                     MsgNextScreen("PRESS ANY KEY TO EXIT");
                     break;
@@ -55,7 +55,7 @@
                     MsgNextScreen("Error. Prem una tecla per tornar al menú...");
                     break;
             }
-        } while (tecla.Key != ConsoleKey.D0);
+        } while (!(tecla.Key == ConsoleKey.D0)||(tecla.Key == ConsoleKey.NumPad0));
     }
 
     public static void ShowOptions()
@@ -78,8 +78,314 @@
         Console.ReadKey();
     }
     //Arnau
+    public static int ValidAnswer()
+    {
+        int answer = 0;
+        try
+        {
+            bool valid = false;
+            Console.Write("\t Answer: ");
+            //Console.Write("\t");
+            while (!valid)
+            {
 
+                ConsoleKeyInfo tecla;
+                tecla = Console.ReadKey();
+                switch (tecla.Key)
+                {
+                    case ConsoleKey.A:
+                        answer = 1;
+                        Console.WriteLine();
+                        Console.WriteLine("A");
+                        valid = true;
+                        break;
+                    case ConsoleKey.B:
+                        Console.WriteLine();
+                        Console.WriteLine("B");
+                        answer = 2;
+                        valid = true;
+                        break;
+                    case ConsoleKey.C:
+                        Console.WriteLine();
+                        Console.WriteLine("C");
+                        answer = 3;
+                        valid=true;
+                        break;
+                    case ConsoleKey.D:
+                        Console.WriteLine();
+                        Console.WriteLine("D");
+                        answer = 4;
+                        valid = true;
+                        break;
+                    default:
+                        answer = 0;
+                        break;
 
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        return answer;
+    }
+    public static void MusicCuriosities()
+    {
+        Console.Clear();
+        int count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0; 
+        double total;
+        count1=MusicQuestion1();
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT QUESTION");
+        count2 = MusicQuestion2();
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT QUESTION");
+        count3 = MusicQuestion3();
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT QUESTION");
+        count4 = MusicQuestion4();
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT QUESTION");
+        count5 = MusicQuestion5();
+        total = count1+ count2+count3+count4+count5;
+        Console.WriteLine("You've got {0} out of 5 questions right", total);
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT MAIN MENU");
+
+    }
+    public static int MusicQuestion1()
+    {
+            Console.Clear();
+            int count = 0;
+            Console.WriteLine("Why does Fall out Boy have it's name?");
+            Console.WriteLine("A - A RANDOM GUY IN THE CROWD SHOUDED THE NAME");
+            Console.WriteLine("B - THEY ARE ALL FANS FROM THE SIMPSONS");
+            Console.WriteLine("C - IS THE VILLAN FROM A COMIC BOOK SERIES");
+            Console.WriteLine("D - THEY CHECKED DIFFERENT COMBINATIONS OF NAMES");
+            if (ValidAnswer() == 1)
+            {
+                Console.WriteLine("CORRECT!");
+                count = 1;
+            }
+            else
+            {
+                Console.WriteLine("SORRY BUT THE RIGHT QUESTION WAS THE LETTER → A");
+                count=0;
+            }
+            return count;
+
+    }
+    public static int MusicQuestion2()
+    {
+        Console.Clear();
+        int count = 0;
+        Console.WriteLine("The singer of what band did not recognize himself on the radio?");
+        Console.WriteLine("A - The Sex Pistols");
+        Console.WriteLine("B - Red Hot Chilli peppers");
+        Console.WriteLine("C - Nickelback");
+        Console.WriteLine("D - Aerosmith");
+        if (ValidAnswer() == 4)
+        {
+            Console.WriteLine("CORRECT!");
+            count = 1;
+        }
+        else
+        {
+            Console.WriteLine("SORRY BUT THE RIGHT QUESTION WAS THE LETTER → D");
+            count = 0;
+        }
+        return count;
+
+    }
+    public static int MusicQuestion3()
+    {
+        Console.Clear();
+        int count = 0;
+        Console.WriteLine("What band rejected Slash's audition because he was too ugly?");
+        Console.WriteLine("A - Europe");
+        Console.WriteLine("B - T.Rex");
+        Console.WriteLine("C - Poison");
+        Console.WriteLine("D - Bon Jovi");
+        if (ValidAnswer() == 3)
+        {
+            Console.WriteLine("CORRECT!");
+            count = 1;
+        }
+        else
+        {
+            Console.WriteLine("SORRY BUT THE RIGHT QUESTION WAS THE LETTER → C");
+            count = 0;
+        }
+        return count;
+;
+    }
+    public static int MusicQuestion4()
+    {
+        Console.Clear();
+        int count = 0;
+        Console.WriteLine("What singer is knwown for volunteering every year to feed the poor on thanks giving?");
+        Console.WriteLine("A - Alice Cooper");
+        Console.WriteLine("B - Bruce Sprignsteen");
+        Console.WriteLine("C - Kenny loggins");
+        Console.WriteLine("D - Myles Kennedy");
+        if (ValidAnswer() == 1)
+        {
+            Console.WriteLine("CORRECT!");
+            count = 1;
+        }
+        else
+        {
+            Console.WriteLine("SORRY BUT THE RIGHT QUESTION WAS THE LETTER → A");
+            count = 0;
+        }
+        return count;
+;
+    }
+    public static int MusicQuestion5()
+    {
+        Console.Clear();
+        int count = 0;
+        Console.WriteLine("The singer of what metal band is a pilot for a comercial airline when he's not with the band?");
+        Console.WriteLine("A - James Hetfield from Metallica");
+        Console.WriteLine("B - James LaBrie from Dream Theater");
+        Console.WriteLine("C - Bruce Dickinson from Iron Maiden");
+        Console.WriteLine("D - Dave Mustaine from Megadeth");
+        if (ValidAnswer() == 3)
+        {
+            Console.WriteLine("CORRECT!");
+            count = 1;
+        }
+        else
+        {
+            Console.WriteLine("SORRY BUT THE RIGHT QUESTION WAS THE LETTER → C");
+            count = 0;
+        }
+        return count;
+
+    }
+    public static void DadJokes()
+    {
+        Console.Clear();
+        int count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0;
+        double total;
+        count1 = DadQuestion1();
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT QUESTION");
+        count2 = DadQuestion2();
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT QUESTION");
+        count3 = DadQuestion3();
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT QUESTION");
+        count4 = DadQuestion4();
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT QUESTION");
+        count5 = DadQuestion5();
+        total = count1 + count2 + count3 + count4 + count5;
+        Console.WriteLine("You've got {0} out of 5 questions right", total);
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT MAIN MENU");
+
+    }
+    public static int DadQuestion1()
+    {
+        Console.Clear();
+        int count = 0;
+        Console.WriteLine("Whath's brown and sticky?");
+        Console.WriteLine("A - Bread Dough");
+        Console.WriteLine("B - A horse bottom");
+        Console.WriteLine("C - A stick");
+        Console.WriteLine("D - Chewing gum");
+        if (ValidAnswer() == 3)
+        {
+            Console.WriteLine("CORRECT!");
+            count = 1;
+        }
+        else
+        {
+            Console.WriteLine("SORRY BUT THE RIGHT QUESTION WAS THE LETTER → C");
+            count = 0;
+        }
+        return count;
+
+    }
+    public static int DadQuestion2()
+    {
+        Console.Clear();
+        int count = 0;
+        Console.WriteLine("What did the buffalo say to his son when he left for college?");
+        Console.WriteLine("A - Bye bye my son");
+        Console.WriteLine("B - Bison");
+        Console.WriteLine("C - I'm going to buy tobaco, i'll be back soon");
+        Console.WriteLine("D - Save the cheerleader, save the world!");
+        if (ValidAnswer() == 2)
+        {
+            Console.WriteLine("CORRECT!");
+            count = 1;
+        }
+        else
+        {
+            Console.WriteLine("SORRY BUT THE RIGHT QUESTION WAS THE LETTER → B");
+            count = 0;
+        }
+        return count;
+
+    }
+    public static int DadQuestion3()
+    {
+        Console.Clear();
+        int count = 0;
+        Console.WriteLine("Who is the fastest brother of Bruce Lee?");
+        Console.WriteLine("A - Patient-Lee");
+        Console.WriteLine("B - Honest-lee");
+        Console.WriteLine("C - Suden-Lee");
+        Console.WriteLine("D - Broco-Lee");
+        if (ValidAnswer() == 3)
+        {
+            Console.WriteLine("CORRECT!");
+            count = 1;
+        }
+        else
+        {
+            Console.WriteLine("SORRY BUT THE RIGHT QUESTION WAS THE LETTER → C");
+            count = 0;
+        }
+        return count;
+        ;
+    }
+    public static int DadQuestion4()
+    {
+        Console.Clear();
+        int count = 0;
+        Console.WriteLine("How do you know when a joke is a 'dad joke'?");
+        Console.WriteLine("A - It's apparent");
+        Console.WriteLine("B - It's bad");
+        Console.WriteLine("C - It's a pun");
+        Console.WriteLine("D - It's dumb");
+        if (ValidAnswer() == 1)
+        {
+            Console.WriteLine("CORRECT!");
+            count = 1;
+        }
+        else
+        {
+            Console.WriteLine("SORRY BUT THE RIGHT QUESTION WAS THE LETTER → A");
+            count = 0;
+        }
+        return count;
+        ;
+    }
+    public static int DadQuestion5()
+    {
+        Console.Clear();
+        int count = 0;
+        Console.WriteLine("What kind of bees produces milk?");
+        Console.WriteLine("A - Cow bees");
+        Console.WriteLine("B - Nicolas Cage, NOT THE BEES!!!");
+        Console.WriteLine("C - Moo bees");
+        Console.WriteLine("D - Boo bees");
+        if (ValidAnswer() == 4)
+        {
+            Console.WriteLine("CORRECT!");
+            count = 1;
+        }
+        else
+        {
+            Console.WriteLine("SORRY BUT THE RIGHT QUESTION WAS THE LETTER → D");
+            count = 0;
+        }
+        return count;
 
     //Eloy
 
