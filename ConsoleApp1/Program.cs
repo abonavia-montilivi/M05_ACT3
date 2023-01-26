93f2173becc52713a81a49ab83c0987f9b763799
abonavia <a2208025@institutmontilivi.cat>
2023-01-26 15:57:17 +0100

    Music done

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -132,30 +132,133 @@
     }
     public static void MusicCuriosities()
     {
-        try
-        {
+        Console.Clear();
+        int count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0; 
+        double total;
+        count1=MusicQuestion1();
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT QUESTION");
+        count2 = MusicQuestion2();
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT QUESTION");
+        count3 = MusicQuestion3();
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT QUESTION");
+        count4 = MusicQuestion4();
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT QUESTION");
+        count5 = MusicQuestion5();
+        total = count1+ count2+count3+count4+count5;
+        Console.WriteLine("You've got {0} out of 5 questions right", total);
+        MsgNextScreen("PRESS A KEY TO GO TO THE NEXT MAIN MENU");
+
+    }
+    public static int MusicQuestion1()
+    {
+            Console.Clear();
+            int count = 0;
             Console.WriteLine("Why does Fall out Boy have it's name?");
             Console.WriteLine("A - A RANDOM GUY IN THE CROWD SHOUDED THE NAME");
             Console.WriteLine("B - THEY ARE ALL FANS FROM THE SIMPSONS");
             Console.WriteLine("C - IS THE VILLAN FROM A COMIC BOOK SERIES");
             Console.WriteLine("D - THEY CHECKED DIFFERENT COMBINATIONS OF NAMES");
             if (ValidAnswer() == 1)
+            {
                 Console.WriteLine("YEP");
+                count = 1;
+            }
             else
+            {
                 Console.WriteLine("NOPE");
-            //Console.Clear(); Clean screen between questions
+                count=0;
+            }
+            return count;
 
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
-        finally
-        {
-            MsgNextScreen("PRESS A KEY TO GO TO THE MAIN MENU");
-        }
     }
+    public static int MusicQuestion2()
+    {
+        Console.Clear();
+        int count = 0;
+        Console.WriteLine("The singer of what band did not recognize himself on the radio?");
+        Console.WriteLine("A - The Sex Pistols");
+        Console.WriteLine("B - Red Hot Chilli peppers");
+        Console.WriteLine("C - Nickelback");
+        Console.WriteLine("D - Aerosmith");
+        if (ValidAnswer() == 4)
+        {
+            Console.WriteLine("YEP");
+            count = 1;
+        }
+        else
+        {
+            Console.WriteLine("NOPE");
+            count = 0;
+        }
+        return count;
 
+    }
+    public static int MusicQuestion3()
+    {
+        Console.Clear();
+        int count = 0;
+        Console.WriteLine("What band rejected Slash's audition because he was too ugly?");
+        Console.WriteLine("A - Europe");
+        Console.WriteLine("B - T.Rex");
+        Console.WriteLine("C - Poison");
+        Console.WriteLine("D - Bon Jovi");
+        if (ValidAnswer() == 3)
+        {
+            Console.WriteLine("YEP");
+            count = 1;
+        }
+        else
+        {
+            Console.WriteLine("NOPE");
+            count = 0;
+        }
+        return count;
+;
+    }
+    public static int MusicQuestion4()
+    {
+        Console.Clear();
+        int count = 0;
+        Console.WriteLine("What singer is knwown for volunteering every year to feed the poor on thanks giving?");
+        Console.WriteLine("A - Alice Cooper");
+        Console.WriteLine("B - Bruce Sprignsteen");
+        Console.WriteLine("C - Kenny loggins");
+        Console.WriteLine("D - Myles Kennedy");
+        if (ValidAnswer() == 1)
+        {
+            Console.WriteLine("YEP");
+            count = 1;
+        }
+        else
+        {
+            Console.WriteLine("NOPE");
+            count = 0;
+        }
+        return count;
+;
+    }
+    public static int MusicQuestion5()
+    {
+        Console.Clear();
+        int count = 0;
+        Console.WriteLine("The singer of what metal band is a pilot for a comercial airline when he's not with the band?");
+        Console.WriteLine("A - James Hetfield from Metallica");
+        Console.WriteLine("B - James LaBrie from Dream Theater");
+        Console.WriteLine("C - Bruce Dickinson from Iron Maiden");
+        Console.WriteLine("D - Dave Mustaine from Megadeth");
+        if (ValidAnswer() == 3)
+        {
+            Console.WriteLine("YEP");
+            count = 1;
+        }
+        else
+        {
+            Console.WriteLine("NOPE");
+            count = 0;
+        }
+        return count;
+
+    }
     //Eloy
 
 
